8b8f7004694b0289966bb6bd8f650c50a98f21b0
Emmanuel <emmanuel-a-guzman@hotmail.com>
2020-04-03 17:48:01 -0300

    problemas de require solucionado

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,11 @@
 require('./config/config')
 
-const express = require('./node_modules/express');
+const express = require('express');
 const app = express();
 
 
 
-const bodyParser = require('./node_modules/body-parser');
+const bodyParser = require('body-parser');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
